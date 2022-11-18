d5fcb3622b255a8622fc3d48e975fd48b64d9df0
dengolden <antoniomarquez1593@gmail.com>
2022-11-18 09:11:51 +0800

    Udah Kelar Cuy!

---
+++ app/Models/state.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class state extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['country_id', 'name'];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+}
